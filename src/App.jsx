d3fe53f84fd6b0c31b0aca09fa4037797aea4206
Ilham593 <sosham127@gmail.com>
2025-07-02 16:49:01 +0700

    feat: Setup react

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="grid h-screen place-content-center bg-gray-900">
+      <h1 className="text-4xl font-bold text-cyan-400">
+        Hello Tailwind CSS v4! 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
